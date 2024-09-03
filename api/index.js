33d1274bab9e91093870c54a6697f690beebe09a
Famouzcoder <olowookerefaith@gmail.com>
2024-09-03 23:32:37 +0100

    Initial user signup api route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,8 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/auth.route.js';
+
 
 
 
@@ -15,9 +17,11 @@ mongoose.connect(process.env.MONGODB).then(() => {
 })
 
 const app = express();
+app.use(express.json());
 
 app.listen(3000, () => {
     console.log('server is running on port 3000!')
 });
 
 app.use('/api/user', userRoutes);
+app.use('/api/auth', authRoutes);
