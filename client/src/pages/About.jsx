94650ff71c3470705bd0833815866a41e614f52f
Famouzcoder <olowookerefaith@gmail.com>
2024-09-20 08:54:48 +0100

    Add complete about pand project page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,28 @@
 
 function About() {
   return (
-    <div>About</div>
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="max-w-2xl mx-auto p-3 text-center">
+        <div>
+          <h1 className="text-3xl font-semibold text-center my-7">
+            About Famouzcoder' Blog
+          </h1>
+          <div className="text-md text-gray-500 flex flex-col gap-6">
+            <p>
+            Welcome to Famouzcoder's Blog! This blog is a personal project by Famouzcoder, a passionate developer eager to share thoughts and insights with the world. As an enthusiast in technology and coding, Famouzcoder enjoys diving into a wide range of topics related to software and innovation.
+            </p>
+
+            <p>
+            Here, you'll find regular articles and tutorials covering areas like web development, software engineering, and programming languages. Famouzcoder is constantly learning and experimenting with new technologies, so expect fresh and insightful content often!
+            </p>
+
+            <p>
+            We invite you to join the conversation by leaving comments and engaging with other readers. You can like, reply, and discuss different perspectives, creating a vibrant learning community where everyone can grow and improve.
+            </p>
+          </div>
+        </div>
+        </div>
+    </div>
   )
 }
 
--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,7 +1,14 @@
+import CallToAction from "../components/CallToAction"
 
 function Projects() {
   return (
-    <div>Projects</div>
+    <div className="min-h-screen max-w-2xl mx-auto flex justify-center items-center flex-col gap-6 p-3">
+      <h1 className="text-3xl font-semibold">Projects</h1>
+      <p className="text-md text-gray-500">
+      Build interactive and dynamic projects while mastering HTML, CSS, and JavaScript, enhancing both your frontend development skills and overall coding proficiency.
+      </p>
+      <CallToAction />
+    </div>
   )
 }
 
