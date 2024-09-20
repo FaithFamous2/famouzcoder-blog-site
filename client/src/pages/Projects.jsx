50e1b89585a6974e4dc28eb476673fe64ca37d29
Famouzcoder <olowookerefaith@gmail.com>
2024-09-20 08:55:58 +0100

    Add complete project page

--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -5,7 +5,7 @@ function Projects() {
     <div className="min-h-screen max-w-2xl mx-auto flex justify-center items-center flex-col gap-6 p-3">
       <h1 className="text-3xl font-semibold">Projects</h1>
       <p className="text-md text-gray-500">
-      Build interactive and dynamic projects while mastering HTML, CSS, and JavaScript, enhancing both your frontend development skills and overall coding proficiency.
+      Build, Create interactive and dynamic projects while mastering HTML, CSS, and JavaScript, enhancing both your frontend development skills and overall coding proficiency.
       </p>
       <CallToAction />
     </div>
