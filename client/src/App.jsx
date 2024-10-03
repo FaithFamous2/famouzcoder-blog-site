c5f46687cc83a3a082568ee95bfb79e61a35fcc2
Famouzcoder <olowookerefaith@gmail.com>
2024-10-03 10:43:00 +0100

    Implement Lazy loading

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,46 +1,55 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
-import Home from './pages/Home'
-import Signin from './pages/Signin'
-import About from './pages/About'
-import Signup from './pages/Signup'
-import Dashboard from './pages/Dashboard'
-import Projects from './pages/Projects'
-import Header from './components/Header'
-import Footer from './components/Footer'
-import PrivateRoute from './components/PrivateRoute'
-import OnlyAdminPrivateRoute from './components/OnlyAdminPrivateRoute'
-import CreatePost from './pages/CreatePost'
-import UpdatePost from './pages/UpdatePost'
-import PostPage from './pages/PostPage'
-import ScrolltoTop from './components/ScrollToTop'
-import Search from './pages/Search'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import PrivateRoute from './components/PrivateRoute';
+import OnlyAdminPrivateRoute from './components/OnlyAdminPrivateRoute';
+import ScrolltoTop from './components/ScrollToTop';
+import React, { lazy, Suspense } from 'react';
+
+// Lazy load the components
+const Home = lazy(() => import('./pages/Home'));
+const Signin = lazy(() => import('./pages/Signin'));
+const About = lazy(() => import('./pages/About'));
+const Signup = lazy(() => import('./pages/Signup'));
+const Dashboard = lazy(() => import('./pages/Dashboard'));
+const Projects = lazy(() => import('./pages/Projects'));
+const CreatePost = lazy(() => import('./pages/CreatePost'));
+const UpdatePost = lazy(() => import('./pages/UpdatePost'));
+const PostPage = lazy(() => import('./pages/PostPage'));
+const Search = lazy(() => import('./pages/Search'));
 
 function App() {
   return (
     <BrowserRouter>
-    <ScrolltoTop />
-    <Header />
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/about' element={<About />} />
-      <Route path='/sign-in' element={<Signin />} />
-      <Route path='/sign-up' element={<Signup />} />
-      <Route path='/search' element={<Search />} />
-      <Route element={<PrivateRoute />}>
-        <Route path='/dashboard' element={<Dashboard />} />
-      </Route>
+      <ScrolltoTop />
+      <Header />
 
-      <Route element={<OnlyAdminPrivateRoute />}>
-        <Route path='/create-post' element={<CreatePost />} />
-        <Route path='/update-post/:postId' element={<UpdatePost />} />
-      </Route>
+      {/* Use Suspense to wrap around lazy-loaded components */}
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/sign-in' element={<Signin />} />
+          <Route path='/sign-up' element={<Signup />} />
+          <Route path='/search' element={<Search />} />
 
-      <Route path='/projects' element={<Projects />} />
-      <Route path='/post/:postSlug' element={<PostPage />} />
-    </Routes>
-    <Footer />
+          <Route element={<PrivateRoute />}>
+            <Route path='/dashboard' element={<Dashboard />} />
+          </Route>
+
+          <Route element={<OnlyAdminPrivateRoute />}>
+            <Route path='/create-post' element={<CreatePost />} />
+            <Route path='/update-post/:postId' element={<UpdatePost />} />
+          </Route>
+
+          <Route path='/projects' element={<Projects />} />
+          <Route path='/post/:postSlug' element={<PostPage />} />
+        </Routes>
+      </Suspense>
+
+      <Footer />
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
